3a208c236dbde34556fc57a08f8ca2cfd3009481
dev_kol <bethelcollins100@gmail.com>
2025-05-27 00:46:18 +0100

    Add rate limiting middleware

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 import paymentRoute from "./routes/payment.route.js";
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: "Too many requests from this IP, please try again later.",
+});
 
+app.use(limiter);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/v1/payment", paymentRoute);
